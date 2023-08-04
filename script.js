9cd6e5f4528de40f677f3c1441d4cbe3cf76ba0c
Philipe <philipe.araujo89@gmail.com>
2023-08-04 17:07:54 -0300

    Atualização de layout

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 
   AOS.init();
+
+const btnWhatsapp = document.querySelector(".btn-whatsapp");
+
+
+btnWhatsapp.addEventListener("click", () => {
+  alert("Vamos agendar seu serviço");
+});
+
+
+
+
+
+
+
+ 
